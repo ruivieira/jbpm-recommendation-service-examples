461a5b853e59a64adae45a0c5c3f10587989e4ff
Rui  Vieira <ruidevieira@googlemail.com>
2019-08-13 13:33:35 +0100

    Update PMML random forest unit tests

--- jbpm-recommendation-pmml-random-forest/src/test/java/org/jbpm/prediction/pmml/PMMLRandomForestPredictionServiceProcessTest.java
+++ jbpm-recommendation-pmml-random-forest/src/test/java/org/jbpm/prediction/pmml/PMMLRandomForestPredictionServiceProcessTest.java
@@ -66,162 +66,31 @@ public class PMMLRandomForestPredictionServiceProcessTest extends AbstractKieSer
 
 
     /**
-     * For this test insert a quantity of true training samples
-     * to verify the random forest class/process is functional.
-     * Expect confidence > 90.0 and "approved" to be true.
+     * The PMML model was trained with 90% of "john" outcomes as "false"
+     * Expect confidence > 0.9 (90.0%) and "approved" to be false.
      */
     @Test
-    public void testRepeatedRandomForestPredictionService() {
+    public void testUserJohn() {
         Map<String, Object> outputs;
         outputs = startAndReturnTaskOutputData("test item", "john", 5, false);
-        for (int i = 0 ; i < 20; i++) {
-            outputs = startAndReturnTaskOutputData("test item", "john", 5, true);
-        }
         assertTrue((double) outputs.get("confidence") > 0.9);
-        assertEquals("true", outputs.get("approved"));
+        assertEquals("false", outputs.get("approved").toString());
     }
 
     /**
-     * Insert an equal number of true and false samples, making
-     * sure the total number of samples is larger than the dataset
-     * size threshold. Since the dataset size
-     * threshold will have been met and the probability of true
-     * and false will be nearly equal, we expect confidence to be
-     * lower than 0.55 (55%).
+     * The PMML model was trained with 90% of "mary" outcomes as "true"
+     * Expect confidence > 0.9 (90.0%) and "approved" to be true.
      */
     @Test
-    public void testEqualProbabilityRandomForestPredictionService() {
-        Map<String, Object> outputs = new HashMap<>();
-
-        for (int i = 0 ; i < 100; i++) {
-            startAndReturnTaskOutputData("test item", "john", 5, false);
-            outputs = startAndReturnTaskOutputData("test item", "john", 5, true);
-        }
-
-        final double confidence = (double) outputs.get("confidence");
-        assertTrue(confidence < 0.55 && confidence > 0.45);
-    }
-
-    /**
-     * Insert a disproportionate partitioning of true and false samples
-     * of a sample set larger than the dataset size threshold. In this
-     * case true will have higher probability and as such we expect
-     * confidence to be high.
-     */
-    @Test
-    public void testUnequalProbabilityRandomForestPredictionService() {
+    public void testUserMary() {
         Map<String, Object> outputs;
-
-        outputs = startAndReturnTaskOutputData("test item", "john", 5, true);
-        for (int i = 0 ; i < 10; i++) {
-            outputs = startAndReturnTaskOutputData("test item", "john", 5, false);
-        }
-        for (int i = 0 ; i < 90; i++) {
-            outputs = startAndReturnTaskOutputData("test item", "john" , 5, true);
-        }
-
-        assertTrue((double) outputs.get("confidence") > 0.85);
-        assertEquals("true", outputs.get("approved"));
+        outputs = startAndReturnTaskOutputData("test item", "mary", 5, false);
+        assertTrue((double) outputs.get("confidence") > 0.9);
+        assertEquals("true", outputs.get("approved").toString());
     }
 
-    /**
-     * This test shows how after testing with a mixed set (3x false, 17x true) how prediction (and probability)
-     * evolve when you then keep sending as "false" value to the outcome.
-     */
-    @Test
-    public void test1() {
-        Map<String, Object> outputs;
 
-        for (int i = 0 ; i < 17; i++) {
-    		outputs = startAndReturnTaskOutputData("test item", "john", 5, true);
-        }
-    	for (int i = 0 ; i < 40; i++) {
-    		outputs = startAndReturnTaskOutputData("test item", "john", 5, false);
-        }
-    }
 
-    /**
-     * shows how after passing min count of 2 input, accuracy goes to > 0.95 (95%) very quickly.
-     */
-    @Test
-    public void test2() {
-        Map<String, Object> outputs;
-
-        for (int i = 0 ; i < 30; i++) {
-    		outputs = startAndReturnTaskOutputData("test item", "john", 5, false);
-            outputs = startAndReturnTaskOutputData("test item", "mary", 5, true);
-        }
-    }
-
-    /**
-     * This test shows how after passing min count of 2 input with 1 irrelevant param switching between 5 possible
-     * values, it takes a while longer to get to high accuracy
-     */
-    @Test
-    public void test3() {
-        Map<String, Object> outputs;
-
-        for (int i = 0 ; i < 50; i++) {
-    		outputs = startAndReturnTaskOutputData("test item", "john", i % 5, false);
-    		outputs = startAndReturnTaskOutputData("test item", "mary", i % 5, true);
-        }
-
-    }
-
-    /**
-     * This test shows how the accuracy goes extremely quickly to 1.0 (100.0%) if you give it the same result initially
-     * it does not even respond to different inputs
-     */
-    @Test
-    public void test4() {
-        Map<String, Object> outputs;
-
-        for (int i = 0 ; i < 10; i++) {
-    		outputs = startAndReturnTaskOutputData("test item", "john", i % 5, false);
-        }
-		outputs = startAndReturnTaskOutputData("test item2", "mary", 10, true);
-        outputs = startAndReturnTaskOutputData("test item2", "mary", 10, true);
-    }
-
-    /**
-     * This test shows how after passing min count of 2 input with 1 irrelevant param switching between 5 possible
-     * values, accuracy of completely new input is extremely high.
-     */
-    @Test
-    public void test5() {
-        Map<String, Object> outputs;
-
-        for (int i = 0 ; i < 50; i++) {
-    		outputs = startAndReturnTaskOutputData("test item", "john", i % 5, false);
-    		outputs = startAndReturnTaskOutputData("test item", "mary", i % 5, true);
-        }
-		outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, true);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 11, false);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 11, false);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, true);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, false);
-        outputs = startAndReturnTaskOutputData("test item", "john", 5, false);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, true);
-    }
-
-    @Test
-    public void test6() {
-        Map<String, Object> outputs;
-
-        for (int i = 0 ; i < 100; i++) {
-            outputs = startAndReturnTaskOutputData("test item", "john", i % 5, !(new Random().nextDouble() < 0.9));
-            outputs = startAndReturnTaskOutputData("test item", "mary", i % 5, new Random().nextDouble() < 0.9);
-            outputs = startAndReturnTaskOutputData("test item", "mary", i % 3, new Random().nextDouble() < 0.9);
-        }
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, true);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 11, false);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 11, false);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, true);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, false);
-        outputs = startAndReturnTaskOutputData("test item", "john", 5, false);
-        outputs = startAndReturnTaskOutputData("test item2", "krisv", 10, true);
-    }
-    
     protected Map<String, Object> startAndReturnTaskOutputData(String item, String userId, Integer level, Boolean approved) {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("item", item);
